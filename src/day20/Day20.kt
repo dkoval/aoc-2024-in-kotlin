b2e686a721d16f174427cafeea1bd1ccd5dea56e
dkoval <koval.dmitriy@gmail.com>
2024-12-20 12:49:58 +0100

    Refactor solutions to Day20 challenge

--- src/day20/Day20.kt
+++ src/day20/Day20.kt
@@ -16,65 +16,7 @@ fun main() {
         fun move(d: Direction): Cell = Cell(row + d.dr, col + d.dc)
     }
 
-    fun part1(input: List<String>, minSavingTime: Int): Int {
-        val grid = input.map { line -> line.toMutableList() }
-        val m = grid.size
-        val n = grid[0].size
-
-        fun traverse(start: Cell): Int {
-            fun bfs(start: Cell): Int {
-                val q = ArrayDeque<Cell>().apply { offer(start) }
-                val visited = mutableSetOf<Cell>(start)
-                var time = 0
-                while (q.isNotEmpty()) {
-                    repeat(q.size) {
-                        val curr = q.poll()
-                        for (d in Direction.entries) {
-                            val next = curr.move(d)
-
-                            if (grid[next.row][next.col] == 'E') {
-                                return time + 1
-                            }
-
-                            if (grid[next.row][next.col] != '#' && next !in visited) {
-                                q.offer(next)
-                                visited += next
-                            }
-                        }
-                    }
-                    time++
-                }
-                error("Cold not reach the ending cell")
-            }
-
-            val baseTime = bfs(start)
-
-            var count = 0
-            for (row in 1 until m - 1) {
-                for (col in 1 until n - 1) {
-                    if (grid[row][col] == '#') {
-                        grid[row][col] = '.'
-                        if (baseTime - bfs(start) >= minSavingTime) {
-                            count++
-                        }
-                        grid[row][col] = '#'
-                    }
-                }
-            }
-            return count
-        }
-
-        for (row in 1 until m - 1) {
-            for (col in 1 until n - 1) {
-                if (grid[row][col] == 'S') {
-                    return traverse(Cell(row, col))
-                }
-            }
-        }
-        return error("Cold not find the starting cell")
-    }
-
-    fun part2(input: List<String>, maxCheatingTime: Int, minSavingTime: Int): Int {
+    fun solve(input: List<String>, maxCheatingTime: Int, minSavingTime: Int): Int {
         val grid = input.map { line -> line.toMutableList() }
         val m = grid.size
         val n = grid[0].size
@@ -106,7 +48,7 @@ fun main() {
                 for ((c2, t2) in times2) {
                     // t1 - the minimum time to reach (row1, col1) from the starting cell
                     // t2 - the minimum time to reach (row2, col2) from the ending cell
-                    // cheatingTime - the time it takes to get from (row1, col1) to (row2, col2) regardless of obstacles
+                    // cheatingTime - the time it takes to get from (row1, col1) to (row2, col2) regardless of the walls
                     val cheatingTime = abs(c1.row - c2.row) + abs(c1.col - c2.col)
                     if (cheatingTime <= maxCheatingTime && baseTime - (t1 + t2 + cheatingTime) >= minSavingTime) {
                         count++
@@ -138,12 +80,26 @@ fun main() {
         return traverse(start, end)
     }
 
+    fun part1(input: List<String>, minSavingTime: Int): Int {
+        // It takes 2 picoseconds to reach 2 from 1 regardless of the wall
+        // Examples 1:
+        // 1 # 2
+        // Example 2:
+        // 1 .
+        // # 2
+        return solve(input, 2, minSavingTime)
+    }
+
+    fun part2(input: List<String>, minSavingTime: Int): Int {
+        return solve(input, 20, minSavingTime)
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day${DAY_ID}/Day${DAY_ID}_test")
-    check(part1(testInput, 2) == 42)
-    check(part2(testInput, 20, 50) == 285)
+    check(part1(testInput, 2) == 44)
+    check(part2(testInput, 50) == 285)
 
     val input = readInput("day$DAY_ID/Day$DAY_ID")
-    part1(input, 100).println()     // answer 1375
-    part2(input, 20, 100).println() // answer 983054
+    part1(input, 100).println() // answer 1375
+    part2(input, 100).println() // answer 983054
 }
